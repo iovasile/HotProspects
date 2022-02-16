6280167dacdfcd1a448b12e16cea680fe96c83a0
Ionut Vasile <email@iovasile.dev>
2022-02-16 20:07:41 +0200

    feature: generate qrcode for user details

--- HotProspects/Views/MeView.swift
+++ HotProspects/Views/MeView.swift
@@ -6,10 +6,45 @@
 //
 
 import SwiftUI
+import CoreImage
+import CoreImage.CIFilterBuiltins
 
 struct MeView: View {
+    @State private var name = "Ionut Vasile"
+    @State private var emailAddress = "email@iovasile.dev"
+    
+    let context = CIContext()
+    let filter = CIFilter.qrCodeGenerator()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Form {
+                TextField("Name", text: $name)
+                    .font(.title)
+                    .textContentType(.name)
+                TextField("Email address", text: $emailAddress)
+                    .font(.title)
+                    .textContentType(.emailAddress)
+                Image(uiImage: generateQRCode(from: "\(name)\n\(emailAddress)"))
+                    .interpolation(.none)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+                    
+            }
+            .navigationTitle("Your code")
+        }
+    }
+    
+    func generateQRCode(from string: String) -> UIImage {
+        filter.message = Data(string.utf8)
+        if let outputImage = filter.outputImage {
+            if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
+                return UIImage(cgImage: cgImage)
+            }
+        }
+        
+        return UIImage(systemName: "xmark.circle") ?? UIImage()
     }
 }
 
